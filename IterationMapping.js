9ddd16d020bf4a5798934a0455ddb6d2ca063a7b
Mikhaella Nadonga <soriaomikhaella@gmail.com>
2024-10-07 20:20:13 +0800

    Add files via upload

---
+++ IterationMapping.js
@@ -0,0 +1,19 @@
+//create a program prompts the user to input maax limit of the loops 
+//use double loop statement to print out map iterations.
+
+let value = prompt("Enter the value of the double loop:2");
+for (let i = ; 0 < 4; i++); {
+  for(let i=0;i<3;i++);{
+    console.log(i+""ii);
+  }
+  
+}
+console.log(0 + 0); //addition returns 0
+console.log(0 + 1); //addition returns 1
+console.log(0 + 2); //addition returns 2
+console.log(1 + 0); //addition returns 1
+console.log(1 + 1); //addition returns 2
+console.log(1 + 2); //addition returns 3
+console.log(2 + 0); // addition returns 2
+console.log(2 + 1); //addition returns 3
+console.log(2 + 3); //addition returns to 4
